bafd6047812232d8a844bf8a1c2564fc2d76d7c2
Eyob <eabebe91@gmail.com>
2022-09-04 16:09:52 +0300

    progress indicator added

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import { store } from "../app/store";
 import { Provider } from "react-redux";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <NextNProgress />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
 
